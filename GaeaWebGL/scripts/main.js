59294d338649fda5b361b6be0ea8fd946768c84a
Ruochen Hua <ruochen.hua@gmail.com>
2018-10-04 15:29:34 +0800

    add simple mesh control

--- GaeaWebGL/scripts/main.js
+++ GaeaWebGL/scripts/main.js
@@ -6,7 +6,6 @@ init();
 animate();
 
 function init() {
-	
     camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 100);
     camera.position.z = 10;
 
@@ -14,11 +13,15 @@ function init() {
 
     geometry = new THREE.BoxGeometry(2, 4, 2);
     var color = new THREE.Color("#7833aa");
-    material = new THREE.MeshLambertMaterial({
-        color: color.getHex()
+    material = new THREE.MeshPhongMaterial({
+        color: color.getHex(),
+        specular: 0x00ff00,
+        shinyness: 20
     });
     //material = new THREE.MeshNormalMaterial();
 
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
     var light = new THREE.PointLight(0xFFFFFF, 1.5);
     light.position.set(10, 0, 80);
     scene.add(light);
@@ -26,20 +29,45 @@ function init() {
     var light = new THREE.PointLight(0xFFFFFF, 1.5);
     light.position.set(25, 0, -80);
     scene.add(light);
-
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-
     renderer = new THREE.WebGLRenderer({
         antialias: true
     });
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    var controls = new THREE.OrbitControls(camera, renderer.domElement);
-    controls.target = new THREE.Vector3(0, 15, 0);
-    controls.maxPolarAngle = Math.PI / 2;
+    init_input_management();
 }
+
+function key_down_handle(e) {
+    window.onkeydown = function(e) {
+        switch (e.code) {
+            case 'KeyW':
+                mesh.position.y += 1.0;
+                break;
+            case 'KeyS':
+                mesh.position.y -= 1.0;
+                break;
+            case 'KeyA':
+                mesh.position.x -= 1.0;
+                break;
+            case 'KeyD':
+                mesh.position.x += 1.0;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+function mouse_down_handle(e) {
+
+}
+
+function init_input_management() {
+    window.onkeydown = key_down_handle;
+    window.onmousedown = mouse_down_handle;
+}
+
 
 function animate() {
 
@@ -49,5 +77,4 @@ function animate() {
     mesh.rotation.y += 0.02;
 
     renderer.render(scene, camera);
-
 }
