f365621cf277aff64b84961c99112e434861d6dc
Ruochen Hua <ruochen.hua@gmail.com>
2018-10-04 17:48:11 +0800

    add simple camera control

--- GaeaWebGL/scripts/main.js
+++ GaeaWebGL/scripts/main.js
@@ -1,6 +1,40 @@
 /*eslint-disable*/
 var camera, scene, renderer;
 var geometry, material, mesh;
+var look_at = [0,0,0];
+
+var mouse_control_ins = new mouse_control();
+
+function mouse_control() {
+    this.last_x = 0;
+    this.last_y = 0;
+    this.press = false;
+}
+
+mouse_control.prototype.mouse_down = function (e) {
+    this.last_x = e.clientX;
+    this.last_y = e.clientY;
+
+    this.press = true;
+}
+
+mouse_control.prototype.mouse_up = function(e) {
+    console.log("mouse up " + e.clientX + " " + e.clientY);
+    this.press = false;
+}
+
+mouse_control.prototype.mouse_move = function(e) {
+    if(this.press) {
+        var delta_x = e.clientX - this.last_x;
+        var delta_y = e.clientY - this.last_y;
+
+        camera.rotation.x += -delta_y * 0.01;
+        camera.rotation.y += -delta_x * 0.01;
+
+        this.last_x = e.clientX;
+        this.last_y = e.clientY;
+    }
+}
 
 init();
 animate();
@@ -8,7 +42,6 @@ animate();
 function init() {
     camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 100);
     camera.position.z = 10;
-
     scene = new THREE.Scene();
 
     geometry = new THREE.BoxGeometry(2, 4, 2);
@@ -35,6 +68,7 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
+
     init_input_management();
 }
 
@@ -59,13 +93,14 @@ function key_down_handle(e) {
     }
 }
 
-function mouse_down_handle(e) {
-
-}
 
 function init_input_management() {
     window.onkeydown = key_down_handle;
-    window.onmousedown = mouse_down_handle;
+    console.log(mouse_control_ins);
+
+    window.onmousedown = mouse_control_ins.mouse_down;
+    window.onmousemove = mouse_control_ins.mouse_move;
+    window.onmouseup = mouse_control_ins.mouse_up;
 }
 
 
@@ -74,7 +109,6 @@ function animate() {
     requestAnimationFrame(animate);
 
     mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
 
     renderer.render(scene, camera);
 }
